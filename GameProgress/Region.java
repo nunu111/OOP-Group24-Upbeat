83b4287be293ae8cdca829f6bd95925b6fc93e5f
nunu111 <nunu111za@hotmail.com>
2023-03-07 14:34:21 +0700

    Add PartnerRegion and AddPartnerRegion to connect hexagon

--- GameProgress/Region.java
+++ GameProgress/Region.java
@@ -5,11 +5,22 @@ public class Region {
     protected long col;
     protected double deposit;
     protected Player owner;
+    protected Region[] PartnerRegion;
     public Region(long _row,long _col){
         this.row = _row ;
         this.col = _col ;
         this.owner = null;
         this.deposit = 0;
+        PartnerRegion = new Region[]{null,null,null,null,null,null};
+    }
+
+    /**
+     *
+     * @param Partner Region that want to add
+     * @param dir 0-up,1-upright,2-downright,3-down,4-downleft,5-upleft
+     */
+    public void AddPartnerRegion(Region Partner, int dir){
+        this.PartnerRegion[dir] =  Partner;
     }
     public void AddDepositToCenter(long init_deposit){
         this.deposit = init_deposit;
